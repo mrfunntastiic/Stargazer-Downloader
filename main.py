6a4b7f31898fc4b60b3a22512ee1a61d76310ff0
mrfunntastiic <mrfunntastiic3rd@gmail.com>
2026-06-29 16:32:40 +0700

    Fix TemplateResponse signature for newer FastAPI/Starlette

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def download_media(url: str, fmt: str, task_id: str):
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @app.post("/api/info")
